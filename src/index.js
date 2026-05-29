050a63acbc126ec7a095bd47c9c06d50d2928628
Prateek <prateek0217@gmail.com>
2026-05-29 16:30:56 +0530

    connecting with the db using sequelize

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express'); 
+const bodyparser = require('body-parser');
 const {PORT} = require('./config/serverconfig');
 const setup_server = async() => {
     const app = express();
+    //body parser setup 
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended : true}));
  
     app.listen(PORT,() =>{  
        console.log(`Server started at ${PORT}`); 
      //  console.log(process.env);
     });
 }
-setup_server();
+setup_server(); 
