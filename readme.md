a8512aaf3db1f4aa39b7eceb08be7ca06eb4515c
Prateek <prateek0217@gmail.com>
2026-06-05 23:28:17 +0530

    added cityrepository function

--- readme.md
+++ readme.md
@@ -1,6 +1,22 @@
 #Welcome to flights and search
+## Project Setup : 
+- clone the project on your local 
+- run "npm install" on your terminal on the same path of your directory.
+-create a ".env" with the essentials listed below: 
+        -"PORT =3000"
 
+- inside the "src/config" folder create a file "config.json" and then add this following code : 
+{
+    "development" :{
+        "username" : "root",
+        "password" : "root123",
+        "database" : "flights_and_search",
+        "host" : "127.0.0.1",
+        "dialect" : "mysql"
+    }
+    }
 
+-Once you have added your db config as listed above, go to the src folder and run the command "npx sequelize db:create"
 
 ## DB design :
  - Airplane table
@@ -8,3 +24,6 @@
  - Airport 
  - city
  
+ - A Flight belongs to one airport but an airplane can be used in multiple flights
+ - A city has many airports but one airport belongs to a city
+ - one airport can have many flights , but one flight should belongs to an airport
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-const express = require('express'); 
+const express = require('express');
 const bodyparser = require('body-parser');
-const {PORT} = require('./config/serverconfig');
-const setup_server = async() => {
+const { PORT } = require('./config/serverconfig');
+const { City } = require('./models/index');
+const CityRepository = require('./repository/city-repo');
+const setup_server = async () => {
     const app = express();
-    //body parser setup 
+    // body parser setup
     app.use(bodyparser.json());
-    app.use(bodyparser.urlencoded({extended : true}));
- 
-    app.listen(PORT,() =>{  
-       console.log(`Server started at ${PORT}`); 
-     //  console.log(process.env);
+    app.use(bodyparser.urlencoded({ extended: true }));
+
+    app.listen(PORT,() => {
+        console.log(`Server started at ${PORT}`);
+        // console.log(process.env);
+        const repo = new CityRepository();
+        repo.createCity({name : "UNA "});
     });
-}
-setup_server(); 
+};
+
+setup_server();
