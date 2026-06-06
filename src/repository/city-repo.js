fbc7e7f71a525b67c8de7fcb744982c0ed376626
Prateek <prateek0217@gmail.com>
2026-06-06 15:24:32 +0530

    completed city repo

--- src/repository/city-repo.js
+++ src/repository/city-repo.js
@@ -6,6 +6,7 @@
             const city = await City.create({name});
             return city;
         } catch(error){
+            console.log("something went wrong on the repository");
             throw{error};
         }
     }
@@ -17,9 +18,37 @@
                     id: cityid
                 }
             });
+            return true;
         } catch(error){
+            console.log("something went wrong on the repository");
+            throw{error};
+        }
+    }
+    async updatecity(cityid,data){
+        try{
+            const city = await city.update(data,{
+                where : {
+                    id : cityid
+                }
+            });
+            return city;
+        }
+        catch(error){
+            console.log("something went wrong on the repository");
             throw(error);
         }
+    }
+     
+    async getcity(cityid){
+        try{
+            const city = await city.findbypk(cityid);
+            return city; 
+        }
+        catch(error){
+            console.log("something went wrong on the repository");
+            throw{error};
+        }
+
     }
    }
 
